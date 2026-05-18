72c4b65debaa4f4541baa0c4a7006762db240a33
Christian Albrecht <christian.albrecht@mayflower.de>
2026-05-18 14:35:32 +0200

    legacy: pkgs: regenerate overlays.nix

--- legacy/overlays.nix
+++ legacy/overlays.nix
@@ -1,7 +1,14 @@
 final: prev:
 let
   gitlab-nix = {
-    #TODO
+    gitlab = final.callPackage ./pkgs/by-name/gi/gitlab/package.nix { };
+    gitaly = final.callPackage ./pkgs/by-name/gi/gitaly/package.nix { };
+    gitlab-shell = final.callPackage ./pkgs/by-name/gi/gitlab-shell/package.nix { };
+    gitlab-pages = final.callPackage ./pkgs/by-name/gi/gitlab-pages/package.nix { };
+    gitlab-workhorse = final.callPackage ./pkgs/by-name/gi/gitlab/gitlab-workhorse/default.nix { };
+    gitlab-container-registry = final.callPackage ./pkgs/by-name/gi/gitlab-container-registry/package.nix { };
+    gitlab-elasticsearch-indexer = final.callPackage ./pkgs/by-name/gi/gitlab-elasticsearch-indexer/package.nix { };
+    gitlab-runner = final.callPackage ./pkgs/by-name/gi/gitlab-runner/package.nix { };
   };
 in
 gitlab-nix // { inherit gitlab-nix; }
