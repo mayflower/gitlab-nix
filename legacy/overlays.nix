b745407a4b9999e46a524b2e1bf87a18836f5ff8
Christian Albrecht <christian.albrecht@mayflower.de>
2026-05-19 18:04:53 +0200

    legacy: pkgs: regenerate overlays.nix

--- legacy/overlays.nix
+++ legacy/overlays.nix
@@ -1,14 +1,14 @@
 final: prev:
 let
   gitlab-nix = {
-    gitlab = final.callPackage ./pkgs/by-name/gi/gitlab/package.nix { };
     gitaly = final.callPackage ./pkgs/by-name/gi/gitaly/package.nix { };
-    gitlab-shell = final.callPackage ./pkgs/by-name/gi/gitlab-shell/package.nix { };
-    gitlab-pages = final.callPackage ./pkgs/by-name/gi/gitlab-pages/package.nix { };
+    gitlab = final.callPackage ./pkgs/by-name/gi/gitlab/package.nix { };
     gitlab-workhorse = final.callPackage ./pkgs/by-name/gi/gitlab/gitlab-workhorse/default.nix { };
     gitlab-container-registry = final.callPackage ./pkgs/by-name/gi/gitlab-container-registry/package.nix { };
     gitlab-elasticsearch-indexer = final.callPackage ./pkgs/by-name/gi/gitlab-elasticsearch-indexer/package.nix { };
+    gitlab-pages = final.callPackage ./pkgs/by-name/gi/gitlab-pages/package.nix { };
     gitlab-runner = final.callPackage ./pkgs/by-name/gi/gitlab-runner/package.nix { };
+    gitlab-shell = final.callPackage ./pkgs/by-name/gi/gitlab-shell/package.nix { };
   };
 in
 gitlab-nix // { inherit gitlab-nix; }
