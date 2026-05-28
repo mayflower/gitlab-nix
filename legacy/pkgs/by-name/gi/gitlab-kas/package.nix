2f4586b804774d2820ffc93b050fd680e6349a20
Christian Albrecht <christian.albrecht@mayflower.de>
2026-05-28 15:25:51 +0200

    legacy: pkgs: import from nixpkgs@11cda25

--- legacy/pkgs/by-name/gi/gitlab-kas/package.nix
+++ legacy/pkgs/by-name/gi/gitlab-kas/package.nix
@@ -7,17 +7,17 @@
 
 buildGoModule (finalAttrs: {
   pname = "gitlab-kas";
-  version = "18.9.7";
+  version = "18.6.3";
 
   # nixpkgs-update: no auto update
   src = fetchFromGitLab {
     owner = "gitlab-org";
     repo = "cluster-integration/gitlab-agent";
     tag = "v${finalAttrs.version}";
-    hash = "sha256-Tcv8mG3mnyD7+J2/lMgbLJ1+GcXkBASKv94YsiREEwE=";
+    hash = "sha256-crKzUQ+51hRgQsICES4tULyrRFw+hjWN9jz+lxYjtew=";
   };
 
-  vendorHash = "sha256-wOj1xIFyU5zvC5Db7q6wenft6xxAbReTtbmOgw+52qo=";
+  vendorHash = "sha256-SSxQH45CVg1v8PKkbU046bAZsZOOPN5U7Cm81n82uRA=";
   subPackages = [ "./cmd/kas" ];
 
   ldflags =
