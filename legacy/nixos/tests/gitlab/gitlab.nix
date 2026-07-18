43c1b03109a24bdbf41d8665307492b1a75121e9
Christian Albrecht <christian.albrecht@mayflower.de>
2026-07-18 18:43:20 +0200

    legacy: nixos: import modules from nixpkgs@d57cfaf

--- legacy/nixos/tests/gitlab/gitlab.nix
+++ legacy/nixos/tests/gitlab/gitlab.nix
@@ -7,12 +7,12 @@
 # - Opening and closing issues.
 # - Downloading repository archives as tar.gz and tar.bz2
 # Run with
-# [nixpkgs]$ nix-build -A nixosTests.gitlab
+# [nixpkgs]$ nix-build -A nixosTests.gitlab.gitlab
 
 { pkgs, lib, ... }:
 
 let
-  inherit (import ./ssh-keys.nix pkgs) snakeOilPrivateKey snakeOilPublicKey;
+  inherit (import ../ssh-keys.nix pkgs) snakeOilPrivateKey snakeOilPublicKey;
   initialRootPassword = "notproduction";
   rootProjectId = "2";
 
@@ -30,15 +30,14 @@ in
 {
   name = "gitlab";
   meta.maintainers = with lib.maintainers; [
-    globin
     yayayayaka
   ];
 
   nodes = {
     gitlab =
-      { ... }:
+      { config, ... }:
       {
-        imports = [ common/user-account.nix ];
+        imports = [ ../common/user-account.nix ];
 
         environment.systemPackages = with pkgs; [ git ];
 
@@ -76,12 +75,19 @@ in
 
         services.dovecot2 = {
           enable = true;
-          enableImap = true;
+          enablePAM = true;
+          settings = {
+            dovecot_config_version = "2.4.3";
+            dovecot_storage_version = config.services.dovecot2.package.version;
+            protocols.imap = true;
+            mail_driver = "maildir";
+            mail_path = "~/mail";
+          };
         };
 
         systemd.services.gitlab-backup.environment.BACKUP = "dump";
 
-        services.gitlab-nix = {
+        services.gitlab = {
           enable = true;
           databasePasswordFile = pkgs.writeText "dbPassword" "xo0daiF4";
           initialRootPasswordFile = pkgs.writeText "rootPassword" initialRootPassword;
@@ -229,7 +235,7 @@ in
         gitlab.wait_for_unit("gitlab.service")
         gitlab.wait_for_unit("gitlab-pages.service")
         gitlab.wait_for_unit("gitlab-sidekiq.service")
-        gitlab.wait_for_file("${nodes.gitlab.services.gitlab-nix.statePath}/tmp/sockets/gitlab.socket")
+        gitlab.wait_for_file("${nodes.gitlab.services.gitlab.statePath}/tmp/sockets/gitlab.socket")
         gitlab.wait_until_succeeds("curl -sSf http://gitlab/users/sign_in")
         gitlab.wait_for_unit("docker-registry.service")
       '';
@@ -487,15 +493,15 @@ in
     + ''
       gitlab.systemctl("start gitlab-backup.service")
       gitlab.wait_for_unit("gitlab-backup.service")
-      gitlab.wait_for_file("${nodes.gitlab.services.gitlab-nix.statePath}/backup/dump_gitlab_backup.tar")
+      gitlab.wait_for_file("${nodes.gitlab.services.gitlab.statePath}/backup/dump_gitlab_backup.tar")
       gitlab.systemctl("stop postgresql gitlab-config.service gitlab.target")
       gitlab.succeed(
-          "find ${nodes.gitlab.services.gitlab-nix.statePath} -mindepth 1 -maxdepth 1 -not -name backup -execdir rm -r {} +"
+          "find ${nodes.gitlab.services.gitlab.statePath} -mindepth 1 -maxdepth 1 -not -name backup -execdir rm -r {} +"
       )
       gitlab.succeed("systemd-tmpfiles --create")
       gitlab.succeed("rm -rf ${nodes.gitlab.services.postgresql.dataDir}")
       gitlab.systemctl("start gitlab-config.service gitaly.service gitlab-postgresql.service")
-      gitlab.wait_for_file("${nodes.gitlab.services.gitlab-nix.statePath}/tmp/sockets/gitaly.socket")
+      gitlab.wait_for_file("${nodes.gitlab.services.gitlab.statePath}/tmp/sockets/gitaly.socket")
       gitlab.succeed(
           "sudo -u gitlab -H gitlab-rake gitlab:backup:restore RAILS_ENV=production BACKUP=dump force=yes"
       )
--- legacy/tests.nix
+++ legacy/tests.nix
@@ -1,7 +1,11 @@
 { pkgs, ... }:
 {
   gitlab-nix = pkgs.testers.runNixOSTest {
-    imports = [ ./nixos/tests/gitlab.nix ];
+    imports = [ ./nixos/tests/gitlab/gitlab.nix ];
+    defaults.imports = [ ./modules.nix ];
+  };
+  gitlab-nix-runner = pkgs.testers.runNixOSTest {
+    imports = [ ./nixos/tests/gitlab/runner.nix ];
     defaults.imports = [ ./modules.nix ];
   };
 }
